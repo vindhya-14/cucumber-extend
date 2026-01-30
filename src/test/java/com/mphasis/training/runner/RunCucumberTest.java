55467a74f83f0689c8f0bf0589bd3ce4349f5e8a
MohanRathod2010 <mohan.ayrathod90@gmail.com>
2026-01-30 10:04:34 +0530

    commit

--- src/test/java/com/mphasis/training/runner/RunCucumberTest.java
+++ src/test/java/com/mphasis/training/runner/RunCucumberTest.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.mphasis.training",
-        plugin = {"pretty"},
+        		 plugin = {
+        			        "pretty",
+        			        "html:target/cucumber/cucumber.html",
+        			        "json:target/cucumber/cucumber.json",
+        			        "rerun:@target/rerun.txt"
+        			    },
         monochrome = true
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
